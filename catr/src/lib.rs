2ad4e29342f64dc4295aa59436fdd7d8cf132042
y-temp4 <y.temp4@gmail.com>
2024-01-28 20:37:37 +0900

    Implement catr

--- catr/src/lib.rs
+++ catr/src/lib.rs
@@ -1,6 +1,6 @@
 use clap::{App, Arg};
 use std::error::Error;
-use std::fs::{read_to_string, File};
+use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 
 #[derive(Debug)]
@@ -59,10 +59,23 @@ pub fn run(config: Config) -> MyResult<()> {
     for filename in config.files {
         match open(&filename) {
             Err(err) => eprintln!("Failed to open {}: {}", filename, err),
-            Ok(_) => {
-                // println!("Opened {}", filename);
-                let content = read_to_string(filename).unwrap();
-                println!("{}", content)
+            Ok(file) => {
+                let mut last_num = 0;
+                for (line_num, line) in file.lines().enumerate() {
+                    let line = line?; // シャドーイング
+                    if config.number_lines {
+                        println!("{:>6}\t{}", line_num + 1, line);
+                    } else if config.number_nonblank_lines {
+                        if !line.is_empty() {
+                            last_num += 1;
+                            println!("{:>6}\t{}", last_num, line);
+                        } else {
+                            println!();
+                        }
+                    } else {
+                        println!("{}", line);
+                    }
+                }
             }
         }
     }
