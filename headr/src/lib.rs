cd4da4696923632d6be26822b35bfea37a7ab36a
y-temp4 <y.temp4@gmail.com>
2024-01-30 21:19:38 +0900

    WIP: Create headr

---
+++ headr/src/lib.rs
@@ -0,0 +1,79 @@
+use clap::{App, Arg};
+use std::error::Error;
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    lines: usize,
+    bytes: Option<usize>,
+}
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+pub fn get_args() -> MyResult<Config> {
+    unimplemented!();
+
+    let matches = App::new("headr")
+        .version("0.1.0")
+        .author("Ken Youens-Clark <kyclark@gmail.com>")
+        .about("Rust head")
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILE")
+                .help("Input file(s)")
+                .multiple(true)
+                .default_value("-"),
+        )
+        .arg(
+            Arg::with_name("number")
+                .short("n")
+                .long("number")
+                .help("Number lines")
+                .takes_value(false)
+                .conflicts_with("number_nonblank"),
+        )
+        .arg(
+            Arg::with_name("number_nonblank")
+                .short("b")
+                .long("number-nonblank")
+                .help("Number non-blank lines")
+                .takes_value(false),
+        )
+        .get_matches();
+
+    // Ok(Config {
+    //     files: matches.values_of_lossy("files").unwrap(),
+    //     lines: matches.is_present("number"),
+    //     bytes: matches.is_present("number_nonblank"),
+    // });
+}
+
+fn parse_positive_int(val: &str) -> MyResult<usize> {
+    match val.parse() {
+        Ok(n) if n > 0 => Ok(n),
+        _ => Err(From::from(val)),
+    }
+}
+
+#[test]
+fn test_parse_positive_int() {
+    // 3は正の整数なのでOK
+    let res = parse_positive_int("3");
+    assert!(res.is_ok());
+    assert_eq!(res.unwrap(), 3);
+
+    // 数字でない文字列の場合はエラー
+    let res = parse_positive_int("foo");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "foo".to_string());
+
+    // 0の場合もエラー
+    let res = parse_positive_int("0");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "0".to_string());
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    println!("{:#?}", config);
+    Ok(())
+}
