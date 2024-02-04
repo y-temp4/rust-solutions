9b04f2cdd5d26ac899c04e13fd8d95c7897b00c6
y-temp4 <y.temp4@gmail.com>
2024-02-04 21:03:37 +0900

    WIP: Implement run fn

--- headr/src/lib.rs
+++ headr/src/lib.rs
@@ -104,7 +104,18 @@ pub fn run(config: Config) -> MyResult<()> {
     for filename in config.files {
         match open(&filename) {
             Err(err) => eprintln!("{}: {}", filename, err),
-            Ok(_) => println!("Opened {}", filename),
+            // Ok(_) => println!("Opened {}", filename),
+            Ok(mut file) => {
+                let mut line = String::new();
+                for _ in 0..config.lines {
+                    let bytes = file.read_line(&mut line)?;
+                    if bytes == 0 {
+                        break;
+                    }
+                    print!("{}", line);
+                    line.clear();
+                }
+            }
         }
     }
     Ok(())
