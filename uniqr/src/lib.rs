03c21656f47f55437127347586a68b2b97ab1a14
y-temp4 <y.temp4@gmail.com>
2024-02-16 20:34:20 +0900

    WIP: Create uniqr

---
+++ uniqr/src/lib.rs
@@ -0,0 +1,11 @@
+use clap::{App, Arg};
+use std::error::Error;
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+    in_file: String,
+    out_file: Option<String>,
+    count: bool,
+}
